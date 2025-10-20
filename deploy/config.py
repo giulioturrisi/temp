bda9f7b5b40d36dad582c74b20267a398dddf08b
gturrisi <giulio.turrisi@iit.it>
2025-10-20 10:32:11 +0200

    added cuncurrent se as standard

--- deploy/config.py
+++ deploy/config.py
@@ -38,7 +38,7 @@ else:
 
 # ----------------------------------------------------------------------------------------------------------------
 
-policy_folder_path = dir_path + "/../tested_policies/" + robot + "/go2_small"
+policy_folder_path = dir_path + "/../tested_policies/" + robot + "/go2_cuncurrent_se"
 #policy_folder_path = dir_path + "/../tested_policies/" + robot + "/go2_5asymm"
 
 cuncurrent_state_est_network = policy_folder_path + "/exported/cuncurrent_state_estimator.pth"
